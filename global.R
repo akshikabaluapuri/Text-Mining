7b34d2c519bfc721a07f13a9e21679599b319100
Akshika Baluapuri <akshikabaluapuri@gmail.com>
2019-07-09 10:13:27 -0400

    Update global.R

--- global.R
+++ global.R
@@ -12,13 +12,3 @@ b <- readRDS(" ngram2.rds")
 t <- readRDS(" ngram3.rds")
 
 
-#wc_file <- input$wc
-#if(!is.null(wc_file)){
- ## wc_text <- readLines(wc_file$datapath)
-#}
-#else
-#{
-  #wc_text <-  c(sampletwitter,sampleblogs,samplenews)
- # wc_text <- "A word cloud is an image of words that together resemles a cloud shape.The size of a
- # word shows how important it is. eg. How often it appear in the text - is frequency."
-#}
